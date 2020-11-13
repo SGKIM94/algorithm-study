f6b5047c8f75d98433942052c66e4cfa9abdf78d
sanggu <tkdrn8578@naver.com>
2020-11-13 23:37:19 +0900

    [ADD] 프로그래머스 - 두개 값 뽑아서 더하기 궇녀

---
+++ src/main/java/search/SumPickedTwoNumbers.java
@@ -0,0 +1,19 @@
+package java.search;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SumPickedTwoNumbers {
+    public int[] solution(int[] numbers) {
+        SortedSet<Integer> sums = new TreeSet<>();
+
+        int numbersSize = numbers.length;
+        for (int i = 0; i < numbersSize - 1; i++) {
+            for (int j = i; j < numbersSize; j++) {
+                sums.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return sums.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

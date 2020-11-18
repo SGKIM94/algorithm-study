af594d3560a9832171d7587acbc83acd72f01e74
sanggu <tkdrn8578@naver.com>
2020-11-18 23:59:11 +0900

    [ADD] 프로그래머스 동적계획법 - 카드게임 구현

---
+++ src/main/java/dinymicPlan/RodCuttings.java
@@ -0,0 +1,39 @@
+package java.dinymicPlan;
+
+import java.util.Arrays;
+
+public class RodCuttings {
+    int[][] dp;
+    public int solution(int[] left, int[] right) {
+        int answer;
+
+        dp = new int[left.length][right.length];
+
+        for (int[] row:dp) {
+            Arrays.fill(row, -1);
+        }
+
+        answer = recur(0, 0, left, right);
+
+        return answer;
+    }
+
+    public int recur(int l, int r, int[] left, int[] right){
+        if (l == left.length || r == right.length) {
+            return 0;
+        }
+
+        if (dp[l][r] != -1) {
+            return dp[l][r];
+        }
+
+        dp[l][r] = Math.max(recur(l+1, r, left, right), recur(l+1, r+1, left, right));
+
+        if (left[l] > right[r]){
+
+            dp[l][r] = Math.max(dp[l][r], recur(l, r+1, left, right) + right[r]);
+        }
+
+        return dp[l][r];
+    }
+}

34b49734f004b119d4372267abb7d19a4f71a57f
sanggu <tkdrn8578@naver.com>
2020-11-21 16:09:44 +0900

    [ADD] 프로그래머스 hash - 위장 구현

---
+++ src/main/java/hash/Camouflage.java
@@ -0,0 +1,27 @@
+package java.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Camouflage {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            if (map.containsKey(clothe[1])) {
+                map.put(clothe[1], map.get(clothe[1]) + 1);
+                continue;
+            }
+
+            map.put(clothe[1], 1);
+        }
+
+        for (Integer integer : map.values()) {
+            answer *= integer + 1;
+        }
+
+        return answer - 1;
+    }
+}

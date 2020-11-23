2657488ee418d46c1355ef388f91195be47cc38c
sanggu <tkdrn8578@naver.com>
2020-11-23 23:49:56 +0900

    [ADD] 프로그래머스 - N 으로 표현 구현
     * 정답을 맞추지 못해 참고 정답 포함

---
+++ src/main/java/dinymicPlan/ExpressByN.java
@@ -0,0 +1,118 @@
+package java.dinymicPlan;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class ExpressByN {
+    class Solution {
+        private List<Integer> answers = new ArrayList<>();
+        private int target;
+        private int source;
+
+        public int solution(int N, int number) {
+            int answer = 0;
+            source = N;
+            target = number;
+
+            String ofSource = String.valueOf(source);
+            for (int i = 0; i < 8; i++) {
+                countExpressTargetBySource(Long.parseLong(ofSource), i);
+                ofSource += ofSource;
+            }
+
+
+            if (answers.size() == 0) {
+                return -1;
+            }
+
+            return Collections.min(answers);
+        }
+
+        public void countExpressTargetBySource(long result, int count) {
+            if (count > 8) {
+                return;
+            }
+
+            if (result == target) {
+                answers.add(count);
+                return;
+            }
+
+
+            int next = count + 1;
+
+            countExpressTargetBySource(result + source, next);
+            countExpressTargetBySource(result - source, next);
+            countExpressTargetBySource(result * source, next);
+            countExpressTargetBySource(result / source, next);
+        }
+    }
+
+
+    class Solution2 {
+        HashSet<Integer> check = new HashSet<Integer>();
+        ArrayList<Integer>[] cache = new ArrayList[9];
+
+        public int add(int left, int right) {
+            return left + right;
+        }
+
+        public int sub(int left, int right) {
+            return left - right;
+        }
+
+        public int mul(int left, int right) {
+            return left * right;
+        }
+
+        public int div(int left, int right) {
+            if (right == 0) return 0;
+            return left / right;
+        }
+
+        public void addCache(int digit, int ret) {
+            if (!check.contains(ret)) {
+                check.add(ret);
+                cache[digit].add(ret);
+            }
+        }
+
+        public void cal(int digit, int left, int right) {
+            addCache(digit, add(left, right));
+            addCache(digit, sub(left, right));
+            addCache(digit, mul(left, right));
+            addCache(digit, div(left, right));
+        }
+
+
+        public int solution(int N, int number) {
+
+            int temp = N;
+            for (int i = 1; i < 9; i++) {
+                if (temp == number) return i;
+                cache[i] = new ArrayList<Integer>();
+                cache[i].add(temp);
+                check.add(temp);
+                temp *= 10;
+                temp += N;
+            }
+
+            for (int digit = 1; digit < 9; digit++) {
+                for (int i = 1; i < digit; i++) {
+                    int j = digit - i;
+                    for (int left : cache[i]) {
+                        for (int right : cache[j]) {
+                            cal(digit, left, right);
+                            if (check.contains(number)) {
+                                return digit;
+                            }
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}

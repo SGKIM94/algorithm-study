5eedd1dde16f241b18b192b6f1dac5f586b0776f
sanggu <tkdrn8578@naver.com>
2020-11-29 21:28:23 +0900

    [ADD] 프로그래머스 동적계획법 - 도둑질 구현

---
+++ src/main/java/dinymicPlan/SteelMoney.java
@@ -0,0 +1,29 @@
+package java.dinymicPlan;
+
+public class SteelMoney {
+    class Solution {
+        public int solution(int[] money) {
+            int countOfHouse = money.length;
+
+            int[] sumOfMoney1 = new int[countOfHouse];
+            int[] sumOfMoney2 = new int[countOfHouse];
+
+            sumOfMoney1[0] = money[0];
+            sumOfMoney1[1] = money[0];
+
+            sumOfMoney2[0] = 0;
+            sumOfMoney2[1] = money[1];
+
+            for (int i = 2; i < countOfHouse; i++) {
+                sumOfMoney1[i] = calculateMoney(sumOfMoney1, i, money);
+                sumOfMoney2[i] = calculateMoney(sumOfMoney2, i, money);
+            }
+
+            return Math.max(sumOfMoney1[countOfHouse - 2], sumOfMoney2[countOfHouse - 1]);
+        }
+
+        private int calculateMoney(int[] sumOfMoney2, int i, int[]  money) {
+            return Math.max(money[i] + sumOfMoney2[i - 2], sumOfMoney2[i - 1]);
+        }
+    }
+}

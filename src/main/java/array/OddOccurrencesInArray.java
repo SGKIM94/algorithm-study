aa5f3e6e9743939d03f56b6ccd941e9848ad3b16
sanggu <tkdrn8578@naver.com>
2020-12-05 18:35:07 +0900

    [ADD] codility - OddOccurrencesInArray 구현

---
+++ src/main/java/array/OddOccurrencesInArray.java
@@ -0,0 +1,48 @@
+package java.array;
+
+import java.util.Arrays;
+
+public class OddOccurrencesInArray {
+    public int solution(int[] A) {
+        int sizeOfNumber = A.length;
+
+        if (sizeOfNumber == 1) {
+            return A[0];
+        }
+
+        Arrays.sort(A);
+
+        int countOfNumber = 1;
+        int prevNumber = A[0];
+
+        for (int i = 1; i < sizeOfNumber; i++) {
+            int number = A[i];
+
+            if (isLastOfIndex(i, sizeOfNumber) && (prevNumber != number)) {
+                return number;
+            }
+
+            if (number != prevNumber) {
+                if (isNotPair(countOfNumber)) {
+                    return prevNumber;
+                }
+
+                prevNumber = number;
+                countOfNumber = 1;
+                continue;
+            }
+
+            countOfNumber++;
+        }
+
+        throw new IllegalArgumentException("Wrong Array!");
+    }
+
+    boolean isNotPair(int countOfNumber) {
+        return countOfNumber % 2 != 0;
+    }
+
+    boolean isLastOfIndex(int index, int sizeOfArray) {
+        return index == sizeOfArray - 1;
+    }
+}

dab741289b5c7ac3792637720d64eb543a7686c5
sanggu <tkdrn8578@naver.com>
2020-12-09 23:47:20 +0900

    [ADD] codility - PermCheck 구현

---
+++ src/main/java/unknown/PermCheck.java
@@ -0,0 +1,25 @@
+package java.unknown;
+
+import java.util.Arrays;
+
+/**
+ * codility
+ * Counting Elements
+ * PermCheck
+ * level : easy
+ */
+public class PermCheck {
+    public int solution(int[] A) {
+        int sizeOfA = A.length;
+
+        Arrays.sort(A);
+
+        for (int i = 0; i < sizeOfA; i++) {
+            if (i + 1 != A[i]) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+}

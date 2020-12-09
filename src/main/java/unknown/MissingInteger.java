54dd67d763d46cf0fdc18e09dd3f4a6545a73d5f
sanggu <tkdrn8578@naver.com>
2020-12-09 21:36:47 +0900

    [ADD] codility - MissingInteger 구현

---
+++ src/main/java/unknown/MissingInteger.java
@@ -0,0 +1,33 @@
+package java.unknown;
+
+import java.util.Arrays;
+
+/**
+ * codility
+ * Counting Elements
+ * MissingInteger
+ * level : medium
+ */
+public class MissingInteger {
+    public int solution(int[] A) {
+        int min = 1;
+
+        Arrays.sort(A);
+
+        for (int integer : A) {
+            if (integer < 1) {
+                continue;
+            }
+
+            if (min == integer) {
+                min++;
+            }
+
+            if (integer > min) {
+                return min;
+            }
+        }
+
+        return min;
+    }
+}

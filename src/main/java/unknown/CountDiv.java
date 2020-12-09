2fefb953cfe3967b6a4eafc299c3c09cf8532758
sanggu <tkdrn8578@naver.com>
2020-12-09 23:47:30 +0900

    [ADD] codility - CountDiv 구현

---
+++ src/main/java/unknown/CountDiv.java
@@ -0,0 +1,26 @@
+package java.unknown;
+
+/**
+ * codility
+ * Prefix Sum
+ * CountDiv
+ * level : medium
+ */
+public class CountDiv {
+    public int solution(int A, int B, int K) {
+        int maxOfQuotient = B / K;
+        int minOfQuotient = calculateMinOfQuotient(A, K);
+
+        return maxOfQuotient - minOfQuotient;
+    }
+
+    private int calculateMinOfQuotient(int A, int K) {
+        int quotient = A / K;
+
+        if (A % K == 0) {
+            return quotient -1;
+        }
+
+        return quotient;
+    }
+}

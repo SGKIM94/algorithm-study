8bf3e89cf79c4c883092368a8683256bab335145
sanggu <tkdrn8578@naver.com>
2020-12-10 23:15:49 +0900

    [ADD] codility - Prefix Sum MinAvgTwoSlice 구현
     * 평균은 최소 값보다 작을 수 없다.
     * 따라서, 원소가 4개인 그룹의 평균은 앞 2개와 뒤 2개씩 나누었을 때의 평균의 작은 값보다
     작아질 수 없으므로 2개인 그룹을 고려하면 4개인 그룹은 확인할 필요가 없다.

---
+++ src/main/java/unknown/MinAvgTwoSlice.java
@@ -0,0 +1,33 @@
+package java.unknown;
+
+/**
+ * codility
+ * Prefix Sum
+ * MinAvgTwoSlice
+ * level : medium
+ * https://nukeguys.tistory.com/175
+ */
+
+public class MinAvgTwoSlice {
+    public int solution(int[] A) {
+        int minIndex = 0;
+        double minOfAverage = (A[0] + A[1]) / 2;
+
+        int sizeOfA = A.length;
+        for (int i = 2; i < sizeOfA; i++) {
+            float average = (A[i - 1] + A[i]) / 2f;
+            if (minOfAverage > average) {
+                minOfAverage = average;
+                minIndex = i - 1;
+            }
+
+            average = (A[i - 2] + A[i - 1] + A[i]) / 3f;
+            if (minOfAverage > average) {
+                minOfAverage = average;
+                minIndex = i - 2;
+            }
+        }
+
+        return minIndex;
+    }
+}
--- src/test/java/search/SearchTest.java
+++ src/test/java/search/SearchTest.java
@@ -1,12 +1,14 @@
 package java.search;
 
 import java.queue.StockPrice;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SearchTest {
     @org.junit.jupiter.api.Test
     public void solution() {
         StockPrice solution = new StockPrice();
-
+        Map<String, String> 123 = new HashMap<>()
 
         String solution = solution.solution(new int[]{3, 30, 34, 5, 9});
 

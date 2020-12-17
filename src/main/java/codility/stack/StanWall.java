a00636c48816afd39ec79839cc5fd4bd2b043691
sanggu <tkdrn8578@naver.com>
2020-12-17 23:30:32 +0900

    [ADD] codlity - Stack And Queue : StanWall 구현

---
+++ src/main/java/codility/stack/StanWall.java
@@ -0,0 +1,24 @@
+package java.codility.stack;
+
+import java.util.Stack;
+
+public class StanWall {
+    public int solution(int[] H) {
+
+        Stack<Integer> pastH = new Stack<>();
+
+        int cnt = 0;
+
+        for(int i : H){
+            while(!pastH.empty() && i < pastH.peek()){
+                pastH.pop();
+            }
+
+            if(pastH.empty() || i > pastH.peek()){
+                pastH.push(i);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}

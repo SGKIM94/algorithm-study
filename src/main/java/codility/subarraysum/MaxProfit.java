f121d55e1b082d66964da4fb2ac5da284cfa7cd1
sanggu <tkdrn8578@naver.com>
2020-12-21 22:19:32 +0900

    [ADD] codlity - Maximum slice problem : MaxProfit - 수정

--- src/main/java/codility/subarraysum/MaxProfit.java
+++ src/main/java/codility/subarraysum/MaxProfit.java
@@ -4,27 +4,32 @@ package java.codility.subarraysum;
  * codility
  * Maximum slice Problem
  * MaxProfit
- * level : medium
- * https://sustainable-dev.tistory.com/25
+ * level : easy
+ * https://sustainable-dev.tistory.com/22
  * 부분합(카데인알고리즘) 에 대한 공부 필요
- * https://juneyr.dev/2019-11-21/maximum-subarray
+ * https://sustainable-dev.tistory.com/23
  */
 public class MaxProfit {
     public int solution(int[] A) {
         int sizeOfA = A.length;
-        int[] profit = new int[A.length];
-        profit[0] = A[0];
 
-        for (int i = 1; i < sizeOfA - 1; i++) {
-            profit[i] = Math.max(0, (A[i] -A[i - 1])- profit[i - 1]);
+        if (sizeOfA == 0 || sizeOfA == 1) {
+            return 0;
         }
 
-        int max = 0;
-        for (int i = 0; i < sizeOfA - 1; i++) {
-            max = Math.max(max, profit[i]);
+        int minProfit = A[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < sizeOfA; i++) {
+            int profit = A[i] - minProfit;
+
+            if (A[i] < minProfit) {
+                minProfit = A[i];
+            }
+
+            maxProfit = Math.max(maxProfit, profit);
         }
 
-        return max;
+        return Math.max(maxProfit, 0);
     }
-
 }

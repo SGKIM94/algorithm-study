79c25f981864f0408d66daee5fd940cd8207edf2
sanggu <tkdrn8578@naver.com>
2021-01-03 23:52:35 +0900

    [MODIFY] 프로그래머스 - 스택/큐 : 기능개발 구현
     * restOfDay 를 구할 때 speed 를 나누지 않는 실수를 범함 주의 필요!!!u

--- src/main/algorithm/programmers/queue/re/FunctionDevelop.java
+++ src/main/algorithm/programmers/queue/re/FunctionDevelop.java
@@ -1,4 +1,82 @@
 package algorithm.programmers.queue.re;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 프로그래머스
+ * 스택 & 큐
+ * FunctionDevelop
+ */
+
 public class FunctionDevelop {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int sizeOfProgresses = progresses.length;
+
+        Queue<Progress> queue = new ArrayDeque<>();
+        for (int i = 0; i < sizeOfProgresses; i++) {
+            queue.offer(new Progress(progresses[i], speeds[i]));
+        }
+
+        List<Integer> countOfDeploys = new ArrayList<>();
+
+        while (!queue.isEmpty() && queue.peek() != null) {
+            int countOfDeploy = 1;
+            Progress currentProgress = queue.poll();
+
+            int restOfDay = currentProgress.getDeployDay();
+
+            while (!queue.isEmpty() && queue.peek() != null
+                    && (queue.peek().calculateProgressAfterDay(restOfDay) >= 100)) {
+                queue.poll();
+                countOfDeploy++;
+            }
+
+            Queue<Progress> afterDayQueue = new ArrayDeque<>();
+            while (!queue.isEmpty() && queue.peek() != null) {
+                Progress progress = queue.poll();
+                afterDayQueue.offer(new Progress(progress.calculateProgressAfterDay(restOfDay), progress.getSpeed()));
+            }
+
+            countOfDeploys.add(countOfDeploy);
+            queue = afterDayQueue;
+        }
+
+        return countOfDeploys.stream().mapToInt(i -> i).toArray();
+    }
+
+    static class Progress {
+        private int current;
+        private int speed;
+
+        Progress(int current, int speed) {
+            this.current = current;
+            this.speed = speed;
+        }
+
+        int getCurrent() {
+            return current;
+        }
+
+        int getSpeed() {
+            return speed;
+        }
+
+        int calculateProgressAfterDay(int day) {
+            return this.current + (this.speed * day);
+        }
+
+        int getDeployDay() {
+            int restOfDay = 100 - this.current;
+            int day = restOfDay / this.speed;
+
+            if (restOfDay % this.speed != 0) {
+                return day + 1;
+            }
+
+            return day;
+        }
+    }
 }

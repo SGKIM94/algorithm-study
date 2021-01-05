13f0a999c366d7c5ba4564320271e319fa2b69d2
sanggu <tkdrn8578@naver.com>
2021-01-05 23:59:22 +0900

    [ADD] 소수찾기 테스트 코드 추가

---
+++ src/test/algorithm/programmers/fullsearch/re/FindPrimeNumberTest.java
@@ -0,0 +1,18 @@
+package algorithm.programmers.fullsearch.re;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindPrimeNumberTest {
+
+    @Test
+    public void solution() {
+        FindPrimeNumber findPrimeNumber = new FindPrimeNumber();
+        int answer = findPrimeNumber.solution("17");
+
+        assertThat(answer, is(3));
+
+    }
+}

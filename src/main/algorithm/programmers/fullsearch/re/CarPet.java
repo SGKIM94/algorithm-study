601c000244761b3f5b570ef2b82ea92f73d151bf
sanggu <tkdrn8578@naver.com>
2021-01-07 00:13:19 +0900

    [MODIFY] 프로그래머스 완저탐색 - 카펫 수정
     * 최대를 기준으로 잡고, yellow 가 공식과 일치하도록 다가간다.

--- src/main/algorithm/programmers/fullsearch/re/CarPet.java
+++ src/main/algorithm/programmers/fullsearch/re/CarPet.java
@@ -5,25 +5,25 @@ package algorithm.programmers.fullsearch.re;
  * 완전탐색
  * 카펫
  * level : 2단계
+ * (x * 2) + ((y - 2) * 2) = 겉표면;
+ * (x - 2) * (y - 2) = 중심 값;
+ * x 가 더 커야하니까 최대를 기준으로 잡고 작업한다.
+ * 따라서 x 는 하나 씩 증가하고 y 는 하나씩 감소한다.
  */
 public class CarPet {
     public int[] solution(int brown, int yellow) {
-        int xy = yellow + brown;
+        int sum = (brown + 4) / 2;
 
-        int x = 3;
         int y = 3;
+        int x = sum - y;
 
         while (true) {
-            if (x * y == xy && x >= y) {
+            if ((x - 2) * (y - 2) == yellow) {
                 return new int[]{x, y};
             }
 
-            if (x < brown) {
-                x++;
-                continue;
-            }
-
             y++;
+            x--;
         }
     }
 }

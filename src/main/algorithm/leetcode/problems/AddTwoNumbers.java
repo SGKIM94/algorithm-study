1c1fb87a69f8ff41292d2891c4d3f113a77391f7
sanggu <tkdrn8578@naver.com>
2021-01-20 23:59:32 +0900

    [MODIFY] 중복 코드 제거 및 extract method

--- src/main/algorithm/leetcode/problems/AddTwoNumbers.java
+++ src/main/algorithm/leetcode/problems/AddTwoNumbers.java
@@ -16,44 +16,50 @@ public class AddTwoNumbers {
      *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
-    class Solution {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            StringBuilder firstNumbers = new StringBuilder(String.valueOf(l1.val));
-            while (l1.next != null) {
-                l1 = l1.next;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        StringBuilder firstNumbers = makeLinkedNumber(l1);
+        StringBuilder secondNumbers = makeLinkedNumber(l2);
 
-                firstNumbers.append(l1.val);
-            }
+        firstNumbers.reverse();
+        secondNumbers.reverse();
 
-            StringBuilder secondNumbers = new StringBuilder(String.valueOf(l2.val));
-            while (l2.next != null) {
-                l2 = l2.next;
+        int sum = Integer.parseInt(firstNumbers.toString()) + Integer.parseInt(secondNumbers.toString());
 
-                secondNumbers.append(l2.val);
-            }
+        String[] numbers = Integer.toString(sum).split("");
 
-            firstNumbers.reverse();
-            secondNumbers.reverse();
-
-            int sum = Integer.parseInt(firstNumbers.toString()) + Integer.parseInt(secondNumbers.toString());
-
-            String[] numbers = Integer.toString(sum).split("");
-
-            ListNode node = new ListNode(Integer.parseInt(numbers[0]), new ListNode());
-
-            for (String number : numbers) {
-                node = new ListNode(Integer.parseInt(number), node);
-            }
-
-            return node;
+        ListNode node = null;
+        for (String number : numbers) {
+            node = new ListNode(Integer.parseInt(number), node);
         }
+
+        return node;
+    }
+
+    private StringBuilder makeLinkedNumber(ListNode l1) {
+        StringBuilder numbers = new StringBuilder(String.valueOf(l1.val));
+        while (l1.next != null) {
+            l1 = l1.next;
+
+            numbers.append(l1.val);
+        }
+
+        return numbers;
     }
 
     static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }

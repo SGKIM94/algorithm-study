5d19497d7b3a4fde114129aa256819157f0cdef0
sanggu <tkdrn8578@naver.com>
2021-01-25 23:59:23 +0900

    [ADD] leetcode - LongestPalindromicSubstring 의 검증 코드 추가

---
+++ src/test/algorithm/leetcode/problems/LongestPalindromicSubstringTest.java
@@ -0,0 +1,33 @@
+package algorithm.leetcode.problems;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongestPalindromicSubstringTest {
+
+    @Test
+    public void longestPalindrome() {
+        LongestPalindromicSubstring solution = new LongestPalindromicSubstring();
+        String answer = solution.longestPalindrome("babad");
+
+        assertEquals(answer, "bab");
+    }
+
+    @Test
+    public void longestPalindrome_second() {
+        LongestPalindromicSubstring solution = new LongestPalindromicSubstring();
+        String answer = solution.longestPalindrome("bb");
+
+        assertEquals(answer, "bb");
+    }
+
+    @Test
+    public void longestPalindrome_third() {
+        LongestPalindromicSubstring solution = new LongestPalindromicSubstring();
+        String answer = solution.longestPalindrome("abb");
+
+        assertEquals(answer, "bb");
+    }
+
+}

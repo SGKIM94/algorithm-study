dd7ecda5591ac4d572e70b9ec843b0a7de28e2a0
sanggu <tkdrn8578@naver.com>
2021-01-26 23:58:37 +0900

    [ADD] leetcode - StringToIntegerAtoi : 구현

---
+++ src/main/algorithm/leetcode/problems/StringToIntegerAtoi.java
@@ -0,0 +1,63 @@
+package algorithm.leetcode.problems;
+
+/**
+ * leetcode
+ * StringToIntegerAtoi
+ * level : easy
+ */
+public class StringToIntegerAtoi {
+    public int myAtoi(String s) {
+        String step1 = s.trim();
+
+        if ("".equals(s)) {
+            return 0;
+        }
+
+        boolean isPositive = true;
+        if (step1.startsWith("-")) {
+            isPositive = false;
+            step1 = step1.replaceFirst("-", "");
+        } else if (step1.startsWith("+")) {
+            step1 = step1.replaceFirst("+", "");
+        }
+
+        String[] words = step1.split("");
+        StringBuilder digits = new StringBuilder();
+
+        for (String word : words) {
+            if (" ".equals(word)) {
+                continue;
+            }
+
+            int asciiOfWord = word.charAt(0);
+
+            if (asciiOfWord < 48 || asciiOfWord > 57) {
+                break;
+            }
+
+            digits.append(word);
+        }
+
+        if (digits.length() == 0) {
+            return 0;
+        }
+
+        int answer;
+
+        try {
+            answer = Integer.parseInt(digits.toString());
+        } catch (NumberFormatException e) {
+            if (!isPositive) {
+                return Integer.MIN_VALUE;
+            }
+
+            return Integer.MAX_VALUE;
+        }
+
+        if (!isPositive) {
+            return answer * -1;
+        }
+
+        return answer;
+    }
+}

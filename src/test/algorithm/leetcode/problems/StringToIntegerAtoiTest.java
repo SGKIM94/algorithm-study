fb250a3a446b7960a50713efeac19c0a5450c787
sanggu <tkdrn8578@naver.com>
2021-01-26 23:58:48 +0900

    [ADD] StringToIntegerAtoi 에 대한 검증 값 추가

---
+++ src/test/algorithm/leetcode/problems/StringToIntegerAtoiTest.java
@@ -0,0 +1,54 @@
+package algorithm.leetcode.problems;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringToIntegerAtoiTest {
+
+    @Test
+    public void myAtoi() {
+        StringToIntegerAtoi solution = new StringToIntegerAtoi();
+
+        int answer = solution.myAtoi("     -42");
+
+        assertEquals(answer, -42);
+    }
+
+    @Test
+    public void myAtoi_second() {
+        StringToIntegerAtoi solution = new StringToIntegerAtoi();
+
+        int answer = solution.myAtoi("4193 with words");
+
+        assertEquals(answer, 4193);
+    }
+
+
+    @Test
+    public void myAtoi_third() {
+        StringToIntegerAtoi solution = new StringToIntegerAtoi();
+
+        int answer = solution.myAtoi("words and 987");
+
+        assertEquals(answer, 0);
+    }
+
+    @Test
+    public void myAtoi_fourth() {
+        StringToIntegerAtoi solution = new StringToIntegerAtoi();
+
+        int answer = solution.myAtoi("-91283472332");
+
+        assertEquals(answer, Integer.MIN_VALUE);
+    }
+
+    @Test
+    public void myAtoi_fifth() {
+        StringToIntegerAtoi solution = new StringToIntegerAtoi();
+
+        int answer = solution.myAtoi("+-12");
+
+        assertEquals(answer, 0);
+    }
+}

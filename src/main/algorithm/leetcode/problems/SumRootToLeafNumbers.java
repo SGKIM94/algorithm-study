2272ecb9445e93ae4e94a7f223451733f6a1b3d1
sanggu <tkdrn8578@naver.com>
2021-02-02 00:02:40 +0900

    [MODIFY] SumRootToLeafNumbers 로직 수정

--- src/main/algorithm/leetcode/problems/SumRootToLeafNumbers.java
+++ src/main/algorithm/leetcode/problems/SumRootToLeafNumbers.java
@@ -1,22 +1,55 @@
 package algorithm.leetcode.problems;
 
-import javax.swing.tree.TreeNode;
-
+/**
+ * leetcode
+ * SumRootToLeafNumbers
+ * level : medium
+ * 다시 풀어보기
+ */
 public class SumRootToLeafNumbers {
     public int sumNumbers(TreeNode root) {
-        if (root == null) return 0;
-        return help(root,0);
+        if (root == null) {
+            return 0;
+        }
+
+        return dfs(root, 0);
     }
 
-    public int help(TreeNode root, int num) {
-        num = num*10 + root.val;
-        if (root.left == null && root.right == null)
+    public int dfs(TreeNode root, int num) {
+        num = num * 10 + root.val;
+
+        if (root.left == null && root.right == null) {
             return num;
-        int l = 0, r = 0;
-        if (root.left != null)
-            l = help(root.left, num);
-        if (root.right != null)
-            r = help(root.right, num);
-        return l + r;
+        }
+
+        int left = 0;
+        int right = 0;
+
+        if (root.left != null) {
+            left = dfs(root.left, num);
+        }
+
+        if (root.right != null) {
+            right = dfs(root.right, num);
+        }
+
+        return left + right;
+    }
+
+    static class TreeNode { int val;
+        private TreeNode left;
+        private TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

0046299dd87953ab26bd832b09aa504e4904360e
sanggu <tkdrn8578@naver.com>
2021-02-03 23:48:41 +0900

    [ADD] leetcode - longestPalindrome : 재 구현

--- src/main/algorithm/leetcode/problems/LongestPalindromicSubstring.java
+++ src/main/algorithm/leetcode/problems/LongestPalindromicSubstring.java
@@ -45,8 +45,8 @@ public class LongestPalindromicSubstring {
         if (start.equals(next)) {
             appended += next;
 
-            if ((depth < sizeOfs - 1 && !start.equals(splited[depth + 1])) &&
-                    (appended.length() > longest.length())) {
+            if (!(depth < sizeOfs - 1 && start.equals(splited[depth + 1]))
+                    && (appended.length() > longest.length())) {
                 longest = appended;
                 return;
             }
@@ -57,5 +57,37 @@ public class LongestPalindromicSubstring {
         }
 
         palindrome(appended + next, start, splited[depth + 1], depth + 1, splited);
+    }
+
+    class Solution {
+        public String longestPalindrome(String s) {
+
+            if (s == null || "".equals(s)) {
+                return s;
+            }
+
+            int len = s.length();
+
+            String ans = "";
+            int max = 0;
+
+            boolean[][] dp = new boolean[len][len];
+
+            for (int j = 0; j < len; j++) {
+
+                for (int i = 0; i <= j; i++) {
+
+                    boolean judge = s.charAt(i) == s.charAt(j);
+
+                    dp[i][j] = j - i > 2 ? dp[i + 1][j - 1] && judge : judge;
+
+                    if (dp[i][j] && j - i + 1 > max) {
+                        max = j - i + 1;
+                        ans = s.substring(i, j + 1);
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }

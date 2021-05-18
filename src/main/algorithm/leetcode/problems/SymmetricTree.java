2f50815686524dc148c359baaa13820c305199b1
sanggu <tkdrn8578@naver.com>
2021-05-18 23:43:08 +0900

    [ADD] leetcode - Symmetric Tree 구현

---
+++ src/main/algorithm/leetcode/problems/SymmetricTree.java
@@ -0,0 +1,41 @@
+package algorithm.leetcode.problems;
+
+/**
+ * leetcode
+ * Symmetric Tree
+ * easy
+ *
+ * https://leetcode.com/problems/symmetric-tree/
+ */
+public class SymmetricTree {
+    private boolean isSymmetric(TreeNode node1, TreeNode node2) {
+
+        if (node1 == null && node2 == null)
+            return true;
+
+        if (node1 == null || node2 == null)
+            return false;
+
+        if (node1.val != node2.val)
+            return false;
+
+        boolean leftright = isSymmetric(node1.left, node2.right);
+        boolean rightleft = isSymmetric(node1.right, node2.left);
+
+        return leftright && rightleft;
+    }
+
+
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
